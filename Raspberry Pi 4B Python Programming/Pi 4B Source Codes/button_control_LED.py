8b2393f52733121e8b90cfe340996e35e4ff6eed
Ly Duc Trung <trung.lyduc18@gmail.com>
2021-11-29 21:25:51 +0700

    Complete 2. Control LED with button tutorial

--- Raspberry Pi 4B Python Programming/Pi 4B Source Codes/button_control_LED.py
+++ Raspberry Pi 4B Python Programming/Pi 4B Source Codes/button_control_LED.py
@@ -2,45 +2,51 @@
 import RPi.GPIO as GPIO
 import time
 
+HIGH = True
+LOW = False
+
+LED = 26
+BUTTON = 19
+
 # Set up GPIO pins
 GPIO.setmode(GPIO.BCM)
-# GPIO.setwarnings(False)
-# pins = [26]
-# for pin in pins:
-GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(26,GPIO.OUT)
-    # GPIO.output(pin,1)
 
-# # Set variables (change these to change behaviour as desired)
-# runcycles = 2
-# ontime = 45 # minutes
-# offtime = 20 #  minutes
+# pull_up_down=GPIO.PUD_DOWN or pull_up_down=GPIO.PUD_UP
+GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+GPIO.setup(LED,GPIO.OUT)
 
-# # Convert run times from minutes to seconds for sleep function
-# ontime *= 60
-# offtime *= 60
-
-# # Run furnace on cycle
-# cycle = 0
-GPIO.output(26,0)
-i = 0
-
-try:
-    while True:
-        # if GPIO.input(19):
-        #     print('Input was HIGH')
-        # # while GPIO.input(19) == GPIO.LOW:
-        # #     time.sleep(0.01)  # wait 10 ms to give CPU chance to do other things
-        if GPIO.input(19):
-            GPIO.output(26,1)
-        else:
-            GPIO.output(26,0)
-        time.sleep(0.01)
-        print(i)
-        i += 1
+def setup():
+    # setup somthing (do 1 time)
+    global i
+    i = 0
+    GPIO.output(LED,LOW)
     
-except KeyboardInterrupt: # if Ctrl C is pressed...
-    pass
-    # GPIO.output(26,0) # shut off the boiler
-    # print("Program stopped and furnace shut off.") # print a clean exit message
-GPIO.cleanup()
+
+def loop():
+    # continuously do something
+    global i, LED, BUTTON
+    
+    try:
+        while True:
+            # Read button state
+            if GPIO.input(BUTTON):
+                GPIO.output(LED,HIGH)
+            else:
+                GPIO.output(LED,LOW)
+            
+            # wait 10 ms to give CPU chance to do other things
+            time.sleep(0.01)
+            print(i)
+            i += 1
+
+    except KeyboardInterrupt: # if Ctrl C is pressed...
+        print("Program stopped and furnace shut off.") # print a clean exit message
+    GPIO.cleanup()
+
+
+def main():
+    setup()
+    loop()
+
+if __name__ == '__main__':
+    main()
